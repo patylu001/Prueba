afdae1fb7de693c30dc1f107b981502bfd642514
Evenel090 <93634867+Evenel090@users.noreply.github.com>
2021-11-29 19:51:36 -0600

    Add files via upload

---
+++ TestCircle/src/test/java/com/fca/calidad/TestCircle/PruebaCoronavirus.java
@@ -0,0 +1,120 @@
+package com.fca.calidad.TestCircle;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class PruebaCoronavirus {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    WebDriverManager.chromedriver().setup();
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testPruebaUady() throws Exception {
+    driver.get("https://es.uadyvirtual.uady.mx/");
+    pause(10000);
+    String textoEjecucion=driver.getTitle();
+    String textorequerido="UADY Virtual Educación Superior: Ingresar al sitio";
+    assertEquals(textoEjecucion,textorequerido);
+  }
+  
+  @Test
+  public void testCredencialesIncorrectas() throws Exception {
+    driver.get("https://es.uadyvirtual.uady.mx/login/index.php");
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("pruebaFallida");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("estonoesmicontra");
+    driver.findElement(By.id("loginbtn")).click();
+    pause(5000);
+    String textoEjecucion=driver.findElement(By.xpath("/html/body/div[1]/div[2]/div/section/div/div[2]/div/div/div/div/div[1]/div")).getText();
+    String textoEsperado="Datos erróneos. Por favor, inténtelo otra vez.";
+    assertThat(textoEsperado,is(textoEsperado));
+  }
+
+  @Test
+  public void testCredencialesCorrectas() throws Exception {
+    driver.get("https://es.uadyvirtual.uady.mx/login/index.php");
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("Matricula");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("*********");
+    driver.findElement(By.id("loginbtn")).click();
+    pause(5000);
+    String textoEjecucion=driver.findElement(By.xpath("/html/body/div[2]/nav/ul[2]/li[2]/div/div/div/div/div/a/span/span[1]")).getText();
+    String textoEsperado="OSKAR OMAR SIERRA GAMBOA";
+    assertThat(textoEsperado,is(textoEsperado));
+  }
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+  private void pause(long mils) {
+	  try {
+		  Thread.sleep(mils);
+	  }catch(Exception e) {
+		  e.printStackTrace();
+	  }
+  }
+}
